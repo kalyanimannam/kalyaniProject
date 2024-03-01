c8e255f88c921f5d0e712088b5b809dbf67a29b3
kalyanimannam@12 <kalyanimannam274@gmail.com>
2024-03-01 15:20:16 +0530

    added login

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Home from './Components.js/Home'
+import Login from './Components.js/Login'
+Home
+function App() {
+  return (
+    <div>
+     <Home></Home>
+     <Login></Login>
+    </div>
+  )
+}
+
+export default App
